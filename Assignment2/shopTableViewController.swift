dafe254809bdcb7c4785a925f83b3bb04eb0ed4f
ALEX RUTH <aruth@10-151-189-137.dhcp-m.deakin.edu.au>
2017-05-16 12:14:57 +1000

    improved price calculation and extending shop

--- Assignment2/shopTableViewController.swift
+++ Assignment2/shopTableViewController.swift
@@ -9,25 +9,25 @@
 import UIKit
 
 
-
 class shopTableViewController: UITableViewController {
     
-    var item1Count : Int64 = 0
-    var item2Count : Int64 = 0
-    var item3Count : Int64 = 0
-    var item4Count : Int64 = 0
+    var item1Count : Float = 0
+    var item2Count : Float = 0
+    var item3Count : Float = 0
+    var item4Count : Float = 0
     
     var CP1 : Float = 1
     var CP2 : Float = 2
-    var CP3 : Float = 1
+    var CP3 : Float = 10
+    var CP4 : Float = 45
     
-    var DP1Count = 0
-    var DP2Count = 0
-    var DP3Count = 0
+    var DP1 = 0
+    var DP2 = 0
+    var DP3 = 0
     
-    var price1 : Float = 10
+    var price1 : Float = 15
     var price2 : Float = 100
-    var price3 : Float = 500
+    var price3 : Float = 1100
     var price4 : Float = 1250
     
     override func viewDidLoad() {
@@ -49,12 +49,16 @@ class shopTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 6
+        return 10
     }
 
     //var ViewController: ViewController?
 
 
+    @IBOutlet weak var clickPowerLabel1: UILabel!
+    @IBOutlet weak var clickPowerLabel2: UILabel!
+    @IBOutlet weak var clickPowerLabel3: UILabel!
+    @IBOutlet weak var clickPowerLabel4: UILabel!
     
     @IBOutlet weak var price1Label: UILabel!
     @IBOutlet weak var price2Label: UILabel!
@@ -67,19 +71,20 @@ class shopTableViewController: UITableViewController {
     
     @IBOutlet weak var doublePower1: UIButton!
     @IBOutlet weak var doublePower2: UIButton!
+    @IBOutlet weak var doublePower3: UIButton!
     
     @IBAction func buyItem1(_ sender: UIButton) {
         
         if currentTomatoes >= price1 {
             perClick = perClick + CP1;
             currentTomatoes = currentTomatoes - price1;
-            price1 = price1 * 1.1;
+            price1 = 15 * powf(1.15, item1Count);
             price1.round()
             item1Count = item1Count + 1;
             price1Label.text = "\(price1)"
             item1PurchaseCount.text = "\(item1Count)"
             
-            if item1Count >= 50 && DP1Count == 0 {
+            if item1Count >= 25 && DP1 == 0 {
                 doublePower1.isEnabled = true
                 doublePower1.isHidden = false
             }
@@ -91,15 +96,34 @@ class shopTableViewController: UITableViewController {
         if currentTomatoes >= price2 {
             perClick = perClick + CP2;
             currentTomatoes = currentTomatoes - price2;
-            price2 = price2 * 1.1;
+            price2 = 100 * powf(1.15, item2Count);
             price2.round()
             item2Count = item2Count + 1;
             price2Label.text = "\(price2)"
             item2PurchaseCount.text = "\(item2Count)"
             
-            if item2Count >= 50 && DP2Count == 0 {
+            if item2Count >= 25 && DP2 == 0 {
                 doublePower2.isEnabled = true
                 doublePower2.isHidden = false
+            }
+        } else {
+            //add popup here
+        }
+    }
+
+    @IBAction func buyItem3(_ sender: UIButton) {
+        if currentTomatoes >= price3 {
+            perClick = perClick + CP3;
+            currentTomatoes = currentTomatoes - price3;
+            price3 = 1100 * powf(1.15, item3Count);
+            price3.round()
+            item3Count = item3Count + 1;
+            price3Label.text = "\(price3)"
+            item3PurchaseCount.text = "\(item3Count)"
+            
+            if item3Count >= 25 && DP3 == 0 {
+                doublePower3.isEnabled = true
+                doublePower3.isHidden = false
             }
         } else {
             //add popup here
@@ -139,21 +163,35 @@ class shopTableViewController: UITableViewController {
         
         if currentTomatoes >= 5000 {
             currentTomatoes = currentTomatoes - 5000
-            perClick = perClick + Float(item1Count)
+            perClick = perClick + (item1Count * CP1)
             CP1 *= 2
+            clickPowerLabel1.text = "+\(CP1) click"
             doublePower1.isEnabled = false
             doublePower1.isHidden = true
-            DP1Count = 1
+            DP1 = 1
         }
     }
     @IBAction func doublePower2(_ sender: UIButton) {
         if currentTomatoes >= 12000 {
             currentTomatoes = currentTomatoes - 12000
-            perClick = perClick + Float(item2Count)
+            perClick = perClick + (item2Count * CP2)
             CP2 *= 2
+            clickPowerLabel2.text = "+\(CP2) click"
             doublePower2.isEnabled = false
             doublePower2.isHidden = true
-            DP2Count = 1
+            DP2 = 1
+            
+        }
+    }
+    @IBAction func doublePower3(_ sender: UIButton) {
+        if currentTomatoes >= 130000 {
+            currentTomatoes = currentTomatoes - 130000
+            perClick = perClick + (item3Count * CP3)
+            CP3 *= 2
+            clickPowerLabel3.text = "+\(CP3) click"
+            doublePower3.isEnabled = false
+            doublePower3.isHidden = true
+            DP3 = 1
             
         }
     }
